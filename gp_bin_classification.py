b37b4ba1e3caea9ab51d15cfb21234d15bb23cb5
srinidhisrinivas <srinidhisrinivas@hotmail.com>
2019-10-21 17:59:53 -0400

    generalized binary classifier

--- gp_bin_classification.py
+++ gp_bin_classification.py
@@ -23,13 +23,15 @@ assert pyro.__version__.startswith('0.4.1');
 pyro.enable_validation(True);
 pyro.set_rng_seed(0);
 
-
 df = pd.read_csv('data_banknote_authentication.txt', delimiter=',');
-x_vals = torch.from_numpy(df.iloc[:,0:4].values).float();
-y_vals = torch.from_numpy(df.iloc[:,4].values).float().reshape(-1,1);
+
+num_features = len(df.columns)-1;
+
+x_vals = torch.from_numpy(df.iloc[:,0:num_features].values).float();
+y_vals = torch.from_numpy(df.iloc[:,num_features].values).float().reshape(-1,1);
 
 train_size = 0.8;
-Xu_size=0.4;
+Xu_size=50;
 np.random.seed(4);
 data = torch.cat((x_vals, y_vals),dim=1);
 
@@ -40,19 +42,20 @@ train_indices = indices[:split];
 test_indices = indices[split:];
 
 train_data = data[train_indices];
-train_x, train_y = train_data[:,0:4], train_data[:,4];
+train_x, train_y = train_data[:,0:num_features], train_data[:,num_features];
 
 test_data = data[test_indices];
-test_x, test_y = test_data[:,0:4], test_data[:,4];
+test_x, test_y = test_data[:,0:num_features], test_data[:,num_features];
 
-kernel = gp.kernels.RBF(input_dim=4);
+kernel = gp.kernels.RBF(input_dim=x_vals.shape[1]);
 likelihood = gp.likelihoods.Binary();
 likelihood.train();
 loss_fn = pyro.infer.Trace_ELBO().differentiable_loss
 
+# Take inducing points at random from training set
 indices = list(range(len(train_x)));
 shuffled_indices = np.random.shuffle(indices);
-split = int(math.floor(Xu_size * len(indices)))
+split = Xu_size;
 Xu = train_x[indices[:split]];
 
 gpr = gp.models.VariationalSparseGP(train_x, train_y, kernel, Xu=Xu, likelihood=likelihood)
@@ -73,11 +76,12 @@ for i in range(num_steps):
 gp.util.train(gpr, num_steps=2500);
 
 with torch.no_grad():
-    pred_f = gpr.forward(test_x)[0];
-    pred = torch.sigmoid(pred_f);
+    pred_f, pred_var = gpr.forward(test_x);
+    pred = gpr.likelihood(pred_f, pred_var);
 
-    pred[pred > 0.5] = 1.;
-    pred[pred < 0.5] = 0.;
+    #pred[pred > 0.5] = 1.;
+    #pred[pred < 0.5] = 0.;
+    #print(pred);
     acc = pred - test_y;
     
     acc_pctg = (acc==0).sum() * 100./len(acc);
--- sklearn_classifier.py
+++ sklearn_classifier.py
@@ -1,7 +1,18 @@
+# Program to compare different sklearn classifiers on datasets
+# Source: https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
+
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 import pandas as pd
 import numpy as np
@@ -10,11 +21,27 @@ import sys
 
 df = pd.read_csv('data_banknote_authentication.txt', delimiter=',');
 
-train_x, test_x, train_y, test_y = train_test_split(df.values[:,0:4], df.values[:,4], train_size=0.05);
+train_x, test_x, train_y, test_y = train_test_split(df.values[:,0:4], df.values[:,4], train_size=0.8);
+names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
+         "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
+         "Naive Bayes", "QDA"]
 
-gpc = GaussianProcessClassifier().fit(train_x, train_y);
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(kernel="linear", C=0.025),
+    SVC(gamma=2, C=1),
+    GaussianProcessClassifier(1.0 * RBF(1.0)),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    MLPClassifier(alpha=1, max_iter=1000),
+    AdaBoostClassifier(),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis()]
 
-pred = gpc.predict(test_x);
-acc = (pred==test_y).sum() * 100.0 / len(pred);
+for name, clf in zip(names,classifiers):
 
-print(acc);
+	clf.fit(train_x, train_y);
+	pred = clf.predict(test_x);
+	acc = (pred==test_y).sum() * 100.0 / len(pred);
+
+	print('{0}: {1:0.3f}%'.format(name, acc));
